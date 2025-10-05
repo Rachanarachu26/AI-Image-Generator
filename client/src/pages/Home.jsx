42f74d701e66b55f59c25d99a2ab3abe678ef0f3
Rachanarachu26 <rachanaraju2005@gmail.com>
2025-10-05 13:18:03 +0530

    first

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../component/Header'
+import Steps from '../component/Steps'
+import Description from '../component/Description'
+import Testimonial from '../component/Testimonial'
+import GenerateBtn from '../component/GenerateBtn'
+
+const Home = () => {
+  return (
+    <div>
+     
+     <Header />
+     <Steps/>
+     <Description />
+     <Testimonial />
+     <GenerateBtn />
+    </div>
+  )
+}
+
+export default Home
